91302b792e59dc03dfff0f70a04877cda46b7be4
RichardMEN11 <richardmenning@googlemail.com>
2023-03-05 20:23:17 +0100

    feat: :sparkles: add calculated traffic light

--- app.py
+++ app.py
@@ -6,9 +6,22 @@ model = pickle.load(open('./models/regr-model.pkl', 'rb'))
 
 app = Flask(__name__)
 
+# TODO: find good values for emissions
+def getTrafficLightForEmission(emission):
+    if emission <= 100:
+        return "green"
+    elif emission <= 200:
+        return "yellow"
+    else:
+        return "red"
+
 @app.route('/predict-emission', methods=['POST'])
 def predict_emission():
     data = request.get_json()
     predicted_val = model.predict([[data['value']]])
     predicted_val_as_list = predicted_val.tolist()
-    return predicted_val_as_list[0]
+    traffic_light = getTrafficLightForEmission(predicted_val_as_list[0])
+    return {
+        "predicted_emission": predicted_val_as_list[0],
+        "traffic_light": traffic_light
+    }
